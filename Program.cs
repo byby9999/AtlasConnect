3237b775510b278a4dcfd8d3f56debcd407ec01d
alexandra-savescu <alexandrabalean92@gmail.com>
2022-02-16 12:10:50 +0200

    Cleanup Main

--- Program.cs
+++ Program.cs
@@ -10,39 +10,9 @@ namespace AtlasConnect
             var settings = MongoClientSettings.FromConnectionString(Configurations.MongoCluster_Chester_GenesisTest_AWS);
             var client = new MongoClient(settings);
 
-
-            //Do what entity you want:
-            /*GenesisBusiness.AddItemBarcodes(client);
-            GenesisInserts.AddLocations(client);
-            GenesisInserts.AddLocationItems(client);
-            GenesisInserts.AddLocationUsers(client);
-            GenesisInserts.AddOrganisationSiteUsers(client);
-            GenesisInserts.AddPatients(client);
-            GenesisInserts.AddPreferenceCards(client);
-            GenesisInserts.AddPreferenceCardItems(client);
-            GenesisInserts.AddPreferenceCardProcedures(client);
-            GenesisInserts.AddPreferenceCardProcedurePacks(client);
-            GenesisInserts.AddPreferenceCardSites(client);
-            GenesisInserts.AddPreferenceCardSurgeons(client);
-            GenesisInserts.AddProcedures(client);
-            GenesisInserts.AddProcedureItems(client);
-            GenesisInserts.AddProcedurePacks(client);
-            GenesisInserts.AddProcedureSurgeons(client);
-            GenesisInserts.AddRolePermissions(client);
-            GenesisInserts.AddSuppliers(client);
-            GenesisInserts.AddSupplierItems(client);
-            GenesisInserts.AddSurgeons(client);
-            GenesisInserts.AddSurgeryItemStatusReasons(client);
-            GenesisInserts.AddSurgerySchedules(client);
-            GenesisInserts.AddSurgeryScheduleSecondaryProcedures(client);
-            GenesisInserts.AddTheatres(client);
-            GenesisInserts.AddTheatreStaffs(client);
-            GenesisInserts.AddSurgeries(client);
-            GenesisInserts.AddSurgeryItems(client);
-            */
-
-            //client.UpdateSurgeries(false);
-           
+            //Inserts: client.Add___
+            //Updates: client.Update___
+            
         }
     }
 }
